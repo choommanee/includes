492ede31e6987b631fc4cffa451771f99b9dbc8b
choommanee <skjoey@MacBook-Pro.local>
2014-11-26 12:23:51 +0700

    edit data
    tattoo

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+// MySQL Hostname / Server (for eg: 'localhost')
+$sql_host = 'localhost';
+
+// MySQL Database Name
+$sql_name = 'vmmax_dev';
+
+// MySQL Database User
+$sql_user = 'root';
+
+// MySQL Database Password
+$sql_pass = '20521@@';
+
+// Site URL
+$site_url = 'http://www.maxengines.com';
+
+// Facebook App Details
+$fb_app_id = ''; // Your Facebook App ID, Create one at http://developers.facebook.com
+$fb_app_secret = ''; // Your Facebook App Secret
+
